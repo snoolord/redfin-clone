4bada3edae038ede2ef2a4b6214b1809cba59a21
Winston <winston.j.zhao@gmail.com>
2016-12-19 22:10:39 -0800

    basic styling

--- frontend/components/homepage/home-bar.jsx
+++ frontend/components/homepage/home-bar.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import Header from './header';
 import { withRouter } from 'react-router';
 import HomeBarLeft from './home-bar-left';
+import MarketingSectionOne from './marketing-section-one';
 
 class HomeBar extends Component {
    constructor(props) {
       super(props);
-
    }
 
    render() {
       return (
-         <div id="home-bar" className="clearfix">
-            <Header/>
-            <HomeBarLeft/>
+         <div className="home-page">
+            <div id="home-bar" className="clearfix">
+               <Header/>
+               <HomeBarLeft/>
+            </div>
+            <MarketingSectionOne />   
          </div>
       );
    }
---
+++ frontend/components/homepage/marketing-section-one.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MarketingSectionOne = () => {
+   return (
+      <section className="marketing-section-one">
+         <div className="section-content">
+            <div className="centered-content">
+               <h1>Full-Service Agents, Modern Technology</h1>
+               <p>
+                  We’re full-service, local agents who get to know you
+                  over coffee and on home tours, and we use online tools
+                  to make you smarter and faster. More than 10,000 customers
+                  buy or sell a home with us each year.
+               </p>
+               <a href='/why-redfin' className="button v3 hero"><span>See Our Story</span></a>
+            </div>
+         </div>
+      </section>
+   );
+};
+
+export default MarketingSectionOne;
