bfca7ba34ee7d1647cab7b3ed22522df61751d35
Winston <winston.j.zhao@gmail.com>
2017-01-05 14:06:26 -0800

    refactor buy sell header

--- frontend/components/homepage/header-buy.jsx
+++ frontend/components/homepage/header-buy.jsx
@@ -8,6 +8,7 @@ class HeaderBuySell extends Component {
 		};
 		this.buyOrSell = this.buyOrSell.bind(this);
 		this.clickLink = this.clickLink.bind(this);
+		this.showList = this.showList.bind(this);
 	}
 
 	clickLink(route) {
@@ -17,27 +18,102 @@ class HeaderBuySell extends Component {
 	}
 
 	buyOrSell() {
-		if (this.props.buy === true) {
+		if ( this.props.buy ) {
 			return (
 				<div className="why-redfin-responsive-buy"
-					onClick={this.clickLink('why-buy')}
-					onMouseEnter={() => this.setState({["showList"]: true})}
-					onMouseLeave={() => this.setState({["showList"]: false})}
 					>Buy ▾
+					<div className="fly-out">
+						{this.showList()}
+					</div>
 				</div>
 			);
 		} else {
 			return (
 				<div className="why-redfin-responsive-sell"
-					onClick={this.clickLink('why-sell')}
 					>Sell ▾
+					<div className="fly-out">
+						{this.showList()}
+					</div>
 				</div>
 			);
 		}
 	}
+
+	showList() {
+		if (this.state.showList && this.props.buy) {
+			return (
+				<ul className="why-buy-list">
+					<li>
+						<ul>
+							<a onClick={this.clickLink('why-buy')}>Buy with Redfin</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('how-much-house-can-i-afford')}>
+								Affordability Calculator
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('home-buying-guide')}>
+								Home Buying Guide
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('buy-a-home/openbook')}>
+								Find Lenders & Inspectors
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('buy-a-home/classes-and-events')}>
+								Classes & Events
+							</a>
+						</ul>
+					</li>
+				</ul>
+			);
+		} else if (this.state.showList && !this.props.buy) {
+			return (
+				<ul className="why-sell-list">
+					<li>
+						<ul>
+							<a onClick={this.clickLink('why-sell')}>Sell with Redfin</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('what-is-my-house-worth')}>
+								What is my home worth
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('sell-a-home/will-selling-pay-off')}>
+								Home Buying Guide
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('sell-a-home/home-selling-guide')}>
+								Home Selling Guide
+							</a>
+						</ul>
+						<ul>
+							<a onClick={this.clickLink('sell-a-home/find-handy-people')}>
+								Find Handypeople & Stagers
+							</a>
+						</ul>
+					</li>
+				</ul>
+			);
+		} else {
+			return (
+				<div></div>
+			);
+		}
+	}
+
 	render() {
 		return (
-			<div className="redfin-menu-item">
+			<div className="redfin-menu-item"
+				onMouseEnter={() => this.setState({["showList"]: true})}
+				onMouseLeave={() => this.setState({["showList"]: false})}
+
+				>
 				{this.buyOrSell()}
 			</div>
 		);
