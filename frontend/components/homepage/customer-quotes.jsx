0bb8bc1a3fe9022d70f5b81bc9c8ae5946552f98
Winston <winston.j.zhao@gmail.com>
2017-01-12 14:35:58 -0800

    created placeholder imgs in array

--- frontend/components/homepage/customer-quotes.jsx
+++ frontend/components/homepage/customer-quotes.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-import Slider from 'react-slick';
 
 class CustomerQuotes extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    img(url) {
+        return (
+            <img src={url}></img>
+        );
+    }
+
+    placeholdIt(slideNum) {
+        return `http://placehold.it/400x300/?text=${slideNum}`;
+    }
+
+    createSlides(num) {
+        let slides = [];
+
+        for (let i = 1; i <= num; i++) {
+            let imgUrl = this.placeholdIt(i);
+            slides.push(imgUrl);
+        }
+        return slides;
+    }
 
     render() {
-        
+        let slides = this.createSlides(5);
+        console.log(slides);
+        return (
+            <div className="customer-quotes">
+                {slides.map((slide) => {
+                    return this.img(slide);
+                })}
+            </div>
+        );
     }
 }
 
