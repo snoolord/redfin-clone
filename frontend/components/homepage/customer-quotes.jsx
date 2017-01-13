4c134ae8f7124bfa54ca4c7d1020549721b8af20
Winston <winston.j.zhao@gmail.com>
2017-01-13 14:27:00 -0800

    add arrows

--- frontend/components/homepage/customer-quotes.jsx
+++ frontend/components/homepage/customer-quotes.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class CustomerQuotes extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            currSlide: 1
+        };
     }
 
     img(url) {
@@ -27,12 +30,22 @@ class CustomerQuotes extends Component {
 
     render() {
         let slides = this.createSlides(5);
-        console.log(slides);
+
         return (
             <div className="customer-quotes">
-                {slides.map((slide) => {
-                    return this.img(slide);
-                })}
+                <button>
+                    <svg width="60px" height="80px" viewBox="0 0 50 80">
+                        <polyline fill="none" stroke="#808080" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="
+                            45.63,75.8 0.375,38.087 45.63,0.375 "/>
+                    </svg>
+                </button>
+                {this.img(slides[this.state.currSlide - 1])}
+                <button>
+                    <svg width="60px" height="80px" viewBox="0 0 50 80">
+                        <polyline fill="none" stroke="#808080" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="
+                            0.375,0.375 45.63,38.087 0.375,75.8 "/>
+                    </svg>
+                </button>
             </div>
         );
     }
